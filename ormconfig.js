cdf443c2c492911fbed12f4d9c1ab6975d7d7fb1
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2021-05-14 21:15:43 -0300

    ormconfig: changed ormconfig to accept ssl

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,13 @@
-console.log('precess.env.DATABASE_URL', process.env.DATABASE_URL)
+console.log("precess.env.DATABASE_URL", process.env.DATABASE_URL);
 module.exports = {
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
-  "entities": ["dist/models/**.js"],
-  "migrations": [
-    "dist/database/migrations/*.js"
-  ],
-  "cli": {
-    "migrationsDir": "src/database/migrations"
-  }
-}
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  entities: ["dist/models/**.js"],
+  migrations: ["dist/database/migrations/*.js"],
+  cli: {
+    migrationsDir: "src/database/migrations",
+  },
+  extra: {
+    ssl: true,
+  },
+};
