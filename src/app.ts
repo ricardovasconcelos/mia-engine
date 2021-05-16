250652cdf409977a693c7a62343ffe314ec71964
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2021-05-15 23:23:19 -0300

    refactor: removed localhost origin from cors access

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://mia-webapp.vercel.app"],
+  origin: "https://mia-webapp.vercel.app",
   optionsSuccessStatus: 200,
 };
 app.use(Cors(corsOptions));
